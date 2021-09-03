5ae9e8ea567b62a6f51bf1a7e17d2a8f68df2203
stkirk <shaanekirk@gmail.com>
2021-09-03 17:04:23 -0400

    used create-react-app to spin up friends app, installed dependencies on friends and server, scaffolded some friends app files

---
+++ friends/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+import Landing from "./components/Landing";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Friends</h1>
+      <Landing />
+    </div>
+  );
+}
+
+export default App;
---
+++ friends/src/components/Landing.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <div className="landing">
+      <h1>LAnding Page</h1>
+    </div>
+  );
+};
+
+export default Landing;
