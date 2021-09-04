c38c7623e9849a0455220f0d9599b403d09016bd
stkirk <shaanekirk@gmail.com>
2021-09-04 18:06:01 -0400

    implemented PrivateRoute for /friends path

--- friends/src/App.js
+++ friends/src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Landing from "./components/Landing";
 import Nav from "./components/Nav";
 import Friends from "./components/Friends";
+import Login from "./components/Login";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
@@ -12,7 +14,8 @@ function App() {
         <Nav />
         <Switch>
           <Route exact path="/" component={Landing} />
-          <Route path="/friends" component={Friends} />
+          <Route path="/login" component={Login} />
+          <PrivateRoute exact path="/friends" component={Friends} />
         </Switch>
       </div>
     </Router>
--- friends/src/components/PrivateRoute.js
+++ friends/src/components/PrivateRoute.js
@@ -1,8 +1,21 @@
 import React from "react";
 import { Redirect, Route } from "react-router";
 
-const PrivateRoute = () => {
-  return <Route />;
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={(props) => {
+        if (localStorage.getItem("token")) {
+          console.log("token found rendering protected route");
+          return <Component {...props} />;
+        } else {
+          console.log("no token found, redirect to login");
+          return <Redirect to="/login" />;
+        }
+      }}
+    />
+  );
 };
 
 export default PrivateRoute;
