dff9c778a2522cb6a489ad5453b784b8d97162b6
stkirk <shaanekirk@gmail.com>
2021-09-05 11:49:18 -0400

    Friends component useEffect fetches protected data from API using axiosWithAuth

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,22 +1,38 @@
 import React, { useEffect } from "react";
 import Friend from "./Friend";
 import { connect } from "react-redux";
+import axiosWithAuth from "../utils/axiosWithAuth";
 
 const Friends = (props) => {
   useEffect(() => {
     console.log("Friends protected route component mounted");
+    axiosWithAuth()
+      .get("/friends")
+      .then((res) => {
+        console.log("axiosWithAuth /friends response: ", res);
+      })
+      .catch((err) => {
+        console.log("axiosWithAuth err: ", err);
+      });
   }, []);
 
   return (
     <div className="friends-list">
       <h1>My Friends</h1>
-      <Friend />
+      {props.loadingFriends ? (
+        <h2>Fetching Friends...</h2>
+      ) : (
+        props.friends.map((friend) => <Friend />)
+      )}
     </div>
   );
 };
 
 const mapStateToProps = (state) => {
-  return {};
+  return {
+    loadingFriends: state.loadingFriends,
+    friends: state.friends,
+  };
 };
 
 export default connect(mapStateToProps, {})(Friends);
